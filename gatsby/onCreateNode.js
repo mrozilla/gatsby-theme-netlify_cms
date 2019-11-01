823681cd07b954d749c80efdcb7005cab198bbd0
jan <jan@mrozilla.cz>
2019-11-01 21:45:12 +0100

    add gatsby node logic

---
+++ gatsby/onCreateNode.js
@@ -0,0 +1,59 @@
+// ─────────────────────────────────────────────────────────────────────────────
+// import
+// ─────────────────────────────────────────────────────────────────────────────
+
+// TODO: finalise for merge into gatsby-plugin-mdx
+const mdx = require('@mdx-js/mdx');
+const babel = require('@babel/core');
+const mapObject = require('map-obj');
+
+// ─────────────────────────────────────────────────────────────────────────────
+// helpers
+// ─────────────────────────────────────────────────────────────────────────────
+
+function mapObjectWithArrays(obj, func) {
+  if (Array.isArray(obj)) {
+    return obj.map((item) => mapObject(item, func, { deep: true }));
+  }
+  return mapObject(obj, func, { deep: true });
+}
+
+function mdxify(key, value) {
+  if (key === 'markdown' || key === 'mdx') {
+    const mdxed = mdx.sync(value);
+    const babeled = babel.transform(mdxed, {
+      configFile: false,
+      // plugins:    [instance.plugin, objRestSpread, htmlAttrToJSXAttr],
+      presets:    [
+        require('@babel/preset-react'),
+        [
+          require('@babel/preset-env'),
+          {
+            useBuiltIns: 'entry',
+            corejs:      2,
+            modules:     'false',
+          },
+        ],
+      ],
+    });
+    const replaced = babeled.code
+      .replace(/export\s*default\s*function\s*MDXContent\s*/, 'return function MDXContent')
+      .replace(/export\s*{\s*MDXContent\s+as\s+default\s*};?/, 'return MDXContent;');
+
+    return [key, replaced];
+  }
+
+  return [key, value];
+}
+
+// ─────────────────────────────────────────────────────────────────────────────
+// component
+// ─────────────────────────────────────────────────────────────────────────────
+
+module.exports = ({ node }) => {
+  if (node.internal.type === 'Mdx') {
+    // TODO: finalise for merge into gatsby-mdx/fork
+    // eslint-disable-next-line no-param-reassign
+    node.frontmatter = mapObjectWithArrays(node.frontmatter, mdxify);
+  }
+};
